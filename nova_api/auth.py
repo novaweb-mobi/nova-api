b4b16c5fd264871f52d5488a78560941b9b4b353
MatTerra <mateus.b.s.terra@gmail.com>
2020-09-10 09:26:58 -0300

    claim validation

--- nova_api/auth.py
+++ nova_api/auth.py
@@ -1,13 +1,42 @@
 import os
+import logging
+from functools import wraps
+from typing import Union
 
 from flask import abort
 from jose import jwt, JWTError
 
 from nova_api import JWT_SECRET
 
+logger = logging.getLogger(__name__)
+
 
 def decode_jwt_token(token):
     try:
         return jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
     except JWTError as e:
-        return abort(401, "Unauthorized")
+        unauthorize()
+
+
+def unauthorize(*args, **kwargs):
+    return abort(401, "Unauthorized")
+
+
+def validate_jwt_claims(token_info: dict = None, **kwargs):
+
+    def make_call(function):
+        for claim_name, claim_value in kwargs.items():
+            if token_info.get(claim_name, None) != claim_value:
+                return unauthorize
+
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            logger.info(
+                "Validating claims on call to %s with token %s and claims: %s",
+                function, token_info, kwargs)
+            print(kwargs)
+            return function(*args, **kwargs)
+
+        return wrapper
+
+    return make_call
--- tests/unittests/test_auth.py
+++ tests/unittests/test_auth.py
@@ -1,6 +1,6 @@
 from jose import jwt
 from werkzeug.exceptions import Unauthorized
-from pytest import raises
+from pytest import mark, raises
 
 import nova_api
 from nova_api import auth
@@ -26,3 +26,28 @@ class TestAuth:
                            nova_api.JWT_SECRET)
         with raises(Unauthorized):
             auth.decode_jwt_token(token)
+
+    @mark.parametrize("claim_name, claim_value, valid", [
+        ("iss", "novaweb.teste", True),
+        ("sub", "tester@novaweb", False),
+        ("custom_claim", "testing", True)
+    ])
+    def test_validate_claims(self, claim_name, claim_value, valid):
+        token_info = {"iss": "novaweb.teste",
+                      "sub": "tester2@novaweb",
+                      "custom_claim": "testing"}
+        kwargs = {claim_name: claim_value}
+
+        @auth.validate_jwt_claims(token_info=token_info, **kwargs)
+        def test_function(keyword=None):
+            return keyword == 1234
+
+        if not valid:
+            with raises(Unauthorized):
+                test_function()
+        else:
+            assert test_function(keyword=1234)
+
+    def test_unauthorize(self):
+        with raises(Unauthorized):
+            auth.unauthorize()
